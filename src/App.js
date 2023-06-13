daae7a4a8e3e5d8cc837bd615d029e62088d72b2
jr-sandbox <136351708+jr-sandbox-github@users.noreply.github.com>
2023-06-13 14:07:34 +0000

    Update 4 files

--- src/App.js
+++ src/App.js
@@ -1,3 +1,29 @@
-export default function Square() {
-  return <button className="square">X</button>;
+import { useState} from 'react';
+
+export function Square() {
+  return <button className="square"></button>;
+}
+
+export default function Board() {
+  
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  
+  return (
+    <>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>);
 }
