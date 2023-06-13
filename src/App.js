da5e5257f8bbb126542f430aae5c38caffa0dd14
jr-sandbox <136351708+jr-sandbox-github@users.noreply.github.com>
2023-06-13 19:16:52 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,79 +1,30 @@
 import { useState} from 'react';
 
 export function Square({ value, onSquareClick }) {
-  return <button className="square" onClick={ onSquareClick }>{ value }</button>;
+  return (
+    <button className="square" onClick={ onSquareClick }>
+      { value }
+    </button>);
 }
 
-export function Board( squares ) {
-  
-  const [winMsg, setWinMsg] = useState("");
+export function Board({xIsNext, squares, onPlay}) {
   
   function handleClick (i) {
-
-    if (winMsg != "") {
-      return;
-    }
-    
-    // slice creates a copy of the array
     const nextSquares = squares.slice();
     
     if (nextSquares[i] != "") {
       return;
     }
     
-    if (nextIsX) {
+    if (xIsNext) 
       nextSquares[i] = "X";
-    }
-    else {
+    else 
       nextSquares[i] = "O";
-    }
-
-    const winningLetter = CheckIfWinner(nextSquares);
-    
-    if (winningLetter != "") {
-      setWinMsg("the winner is " + winningLetter);
-    }
-    
-    //onPlay(nextSquares);
-  }
-  
-  function CheckIfWinner(nextSquares) {
-    
-    const winners = [
-      [0,1,2],
-      [3,4,5],
-      [6,7,8],
-      [0,3,6],
-      [1,4,7],
-      [2,5,8],
-      [0,4,8],
-      [2,4,6]
-    ];
-    
-    for(let idx = 0; idx < winners.length; idx++){
-      let winner = winners[idx];
-      
-      let cellA = nextSquares[winner[0]];
-      if (cellA == "") continue;
-      
-      let cellB = nextSquares[winner[1]];
-      if (cellB == "") continue;
-      
-      let cellC = nextSquares[winner[2]];
-      if (cellC == "") continue;
-      
-      if (cellA != cellB) continue;
-      if (cellB != cellC) continue;
-      
-      return cellA;
-    }
-    
-    return "";
+    onPlay(nextSquares);
   }
   
   return (
     <>
-      <div>{winMsg}</div>
       <div className="board-row">
         <Square value={ squares[0] } onSquareClick={() => handleClick(0)} />
         <Square value={ squares[1] } onSquareClick={() => handleClick(1)} />
@@ -93,24 +44,21 @@ export function Board( squares ) {
 }
 
 export default function Game() {
-  
-  const [nextIsX, setNextIsX] = useState(true);
-  const [currentSquares, setCurrentSquares] = useState(Array(9).fill(""));
-  
-  function handleOnPlay(nextSquares) {
-    setCurrentSquares(nextSquares);
-    setNextIsX(!nextIsX);
+  const [history, setHistory] = useState([Array(9).fill("")]);
+  const [xIsNext, setxIsNext] = useState(true);
+
+  function handlePlay(nextSquares) {
+    setHistory([...history, nextSquares]); // java spread operator
+    setxIsNext(!xIsNext);
   }
-  
+
   return (
-    <>
-      <div className="game">
-        <div className="game-board">
-            <Board squares={currentSquares}/>
-        </div>
-        <div className="game-info" >
-            hello world
-        </div>
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={history[history.length- 1]} onPlay={handlePlay} />
       </div>
-    </>);
+      <div className="game-info">
+      </div>
+    </div>
+  );
 }
